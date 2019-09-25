f0b203eb21fe6092adc265700fdddbe711dcff8a
KLZ-0 <adrian@kalazi.com>
2019-09-25 22:59:53 +0200

    changed timetable.db location to relative

--- config/config.template.py
+++ config/config.template.py
@@ -1,9 +1,11 @@
+import os
+
 class Config:
     key = ""
     command_prefix = "!"
 
     extensions = ["base", "cmds", "timetable", "parser"]
 
-    tt_db = "timetable.db"
+    tt_db = os.path.join(os.path.dirname(__file__), "timetable.db")
     bot_room_id = 624919976561737728
     bot_dev_id = 624895168839024647
