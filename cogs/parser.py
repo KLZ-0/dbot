e32bfeeb1604fc81adc90429a2553d36cdc8e8f9
KLZ-0 <adrian@kalazi.com>
2019-11-28 22:59:14 +0100

    Added pin on reaction

--- cogs/parser.py
+++ cogs/parser.py
@@ -18,7 +18,6 @@ class Parser(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-
         if not message.author.bot:
             # React to user messages only
             if messages.uhoh in message.content.lower():
@@ -26,6 +25,15 @@ class Parser(commands.Cog):
 
             elif "PR" in message.content:
                 await message.channel.send(messages.pr_meme)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if str(payload.emoji) == "📌":
+            guild = self.bot.get_guild(config.guild_id)
+            channel = guild.get_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            if (message.pinned): return
+            await message.pin()
 
 def setup(bot):
     bot.add_cog(Parser(bot))
